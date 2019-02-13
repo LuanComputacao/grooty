d2431d38534dab334902e3f7366ab65cdb2dc81a
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2019-02-13 16:43:10 -0200

    showing example into the home page

--- app/services/WelcomeService.php
+++ app/services/WelcomeService.php
@@ -9,13 +9,15 @@
 namespace Services;
 
 use Models\ClientModel;
+use Kernel\managers\DebugManager;
 
 class WelcomeService
 {
     public static function getWelcomeService(string $origin, string $token): string
     {
-        $client = ClientModel::objects()->getByCtSiteUrlAndCtTokenApi($origin, $token);
 
+        $client = ClientModel::objects()->getByCtSiteUrlAndCtTokenApi($origin, $token);
+        
         return ClientService::getWelcomeService($client);
     }
 
